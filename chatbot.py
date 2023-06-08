ebe572a2b08b62cdceaafb9ac7fe296d3bd5bc5e
Akash Gupta <78719937+Akashthepro@users.noreply.github.com>
2023-06-08 19:37:03 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,27 +5,25 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.uix.label import MDLabel
-from kivy.properties import StringProperty, NumericProperty 
+from kivy.properties import StringProperty, NumericProperty
 from kivy.core.text import LabelBase
 from news import get_news
 from weather import get_current_weather
 from openAPI import chat_Completion
 
-
+Window.Size = (400, 555)
 class Command(MDLabel):
     text = StringProperty()
-    size_hint_x =  NumericProperty()
+    size_hint_x = NumericProperty()
     halign = StringProperty()
     font_name = "Sk-Modernist-Regular"
     font_size = 16
-
 class Response(MDLabel):
     text = StringProperty()
-    size_hint_x =  NumericProperty()
+    size_hint_x = NumericProperty()
     halign = StringProperty()
     font_name = "Sk-Modernist-Regular"
     font_size = 16
-
 class ChatBot(MDApp):
     def change_screen(self, name):
         screen_manager.current = name
@@ -40,20 +38,15 @@ class ChatBot(MDApp):
         """This method links python code with kv files."""
         return screen_manager
 
-    def bot_name(self):
-        if screen_manager.get_screen('main').bot_name.text != "":
-            screen_manager.get_screen('chats').bot_name.text = screen_manager.get_screen('main').bot_name.text
-            screen_manager.current = "chats"
-            """This method sets bot name."""
-
     def response(self, *args):
         query = value
-        greeting = ["hello","hi","hey"]
-        greeting_response = ["Hello! How can I assist you today?","Hey!, How may I help you?"]
-        goodbye_response = ["Goodbye!, have a great day..","GoodBye!"]
-        goodbye = ["bye","goodbye"]
-        Thanks = ["thankyou","thank you so much","thanks alot"]
-        thanks_response = ["Welcome!, Is there any anything else you want?","I am glad you are satisfied with the response.."]
+        greeting = ["hello", "hi", "hey"]
+        greeting_response = ["Hello! How can I assist you today?", "Hey!, How may I help you?"]
+        goodbye_response = ["Goodbye!, have a great day..", "GoodBye!"]
+        goodbye = ["bye", "goodbye"]
+        Thanks = ["thankyou", "thank you so much", "thanks alot"]
+        thanks_response = ["Welcome!, Is there any anything else you want?",
+                           "I am glad you are satisfied with the response.."]
         if query.lower() in greeting:
             response = random.choice(greeting_response)
         elif query.lower() in goodbye:
@@ -78,35 +71,37 @@ class ChatBot(MDApp):
             response = get_current_weather("Jammu")
             # print(response)
         else:
-            response = chat_Completion(query)
-            #print(response)
-        screen_manager.get_screen('chats').chat_list.add_widget(Response(text=response,size_hint_x = .80))
+            response = "Sorry"
+            #response = chat_Completion(query)
+            # print(response)
+        screen_manager.get_screen('chats').chat_list.add_widget(Response(text=response, size_hint_x=.80))
         """This method will generates response ."""
 
     def send(self):
         global size, halign, value
         if screen_manager.get_screen('chats').text_input != "":
             value = screen_manager.get_screen('chats').text_input.text
-            if len(value) <6:
+            if len(value) < 6:
                 size = .22
                 halign = "center"
-            elif len(value) <11:
+            elif len(value) < 11:
                 size = .32
                 halign = "center"
-            elif len(value) <16:
+            elif len(value) < 16:
                 size = .45
                 halign = "center"
-            elif len(value) <21:
+            elif len(value) < 21:
                 size = .58
                 halign = "center"
-            elif len(value) <26:
+            elif len(value) < 26:
                 size = .71
                 halign = "center"
             else:
                 size = .77
                 halign = "left"
-            screen_manager.get_screen('chats').chat_list.add_widget(Command(text=value,size_hint_x=size, halign = "halign"))
-            Clock.schedule_once(self.response,2)
+            screen_manager.get_screen('chats').chat_list.add_widget(
+                Command(text=value, size_hint_x=size, halign="halign"))
+            Clock.schedule_once(self.response, 2)
             screen_manager.get_screen('chats').text_input.text = ""
 
 if __name__ == '__main__':
